82b08aa76a878f093c182e5bfd1d8cb98cb08168
sangunlee6 <sangunlee6@gmail.com>
2025-07-26 22:48:34 +0900

    first commit

---
+++ src/main/java/demo/mini_WMS/MiniWmsApplication.java
@@ -0,0 +1,13 @@
+package demo.mini_WMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniWmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniWmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/mini_WMS/MiniWmsApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.mini_WMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniWmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
